e93baf27612b4d6e246dea2f6a9a254814195811
Sunny Mistry <sunnymis@buffalo.edu>
2016-12-15 22:04:07 -0500

    Responsive side nav bar complete

--- src/js/router.js
+++ src/js/router.js
@@ -17,3 +17,18 @@ function onAboutClick() {
 function onTitleClick() {
     swap(landingContainer,aboutContainer);
 }
+
+function onHamburgerClick() {
+    var ul = document.getElementsByClassName('side-nav-list')[0];
+    if (ul.className === 'side-nav-list') {
+        ul.className += " responsive";
+    } else {
+        ul.className = "side-nav-list";
+    }
+    var socialMediaUl = document.getElementsByClassName('social-media-list')[0];
+    if (socialMediaUl.className === 'social-media-list') {
+        socialMediaUl.className += " responsive";
+    } else {
+        socialMediaUl.className = "social-media-list";
+    }
+}
