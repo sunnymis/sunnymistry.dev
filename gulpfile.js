b7244637d713706d381107419177fc1eea47cda8
Sunny Mistry <sunnymis@buffalo.edu>
2017-01-07 17:39:22 -0500

    Updated build process to include live reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,58 +2,24 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
+const child = require('child_process');
 
-gulp.task('sass', function () {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .on('error', onError)
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+const siteRoot = '_site';
+const cssFiles = 'css/**/*.?(s)css';
+
+gulp.task('jekyll', function() {
+    child.spawn('jekyll', ['serve', '--watch']);
 });
 
-gulp.task('buildCSS',['sass'], function() {
-    return gulp.src(['src/css/**/*.css','src/css/style.min.css'])
-        .pipe(concat('style.min.css'))
-        .on('error', onError)
-        .pipe(gulp.dest('src/dist'))
-        .pipe(browserSync.stream({match: '**/*.css'}));
-});
-
-gulp.task('watch', ['buildCSS','browserSync'], function () {
-    gulp.watch('src/scss/**/*.scss', ['buildCSS']);
-    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
-
-gulp.task('jekyllWatch', ['browser-sync'], function () {
-    gulp.watch("*.scss").on('change', browserSync.reload);
-});
-
-gulp.task('browserSync', function () {
+gulp.task('serve', function() {
     browserSync.init({
-        injectChanges: true,
-       server: {
-           baseDir: './'
-       }
-    });
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+        baseDir: siteRoot
+    }
 });
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
 });
 
-
-gulp.task('default',['watch']);
-
-gulp.task('build', ['jekyllWatch']);
-
-function onError(err) {
-    console.log(err);
-    this.emit('end');
-}
+gulp.task('build', ['jekyll', 'serve']);
