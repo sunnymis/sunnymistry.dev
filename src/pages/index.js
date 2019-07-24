238768642d2221ec23caa1e4dda7e44e9c834d82
Sunny Mistry <sunnyvmistry@gmail.com>
2019-07-23 23:19:36 -0400

    Add Homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet"
 import Timeline from '../components/Timeline';
 import Nav from '../components/Nav';
 import '../styles/main.scss';
+import '../styles/home.scss';
 
 export default () => {
   return (
@@ -14,7 +15,27 @@ export default () => {
 
       <div className="container">
         <Nav />
-        <h1>Home</h1>
+        <div className="content">
+          <p className="bio">
+            I am a Software Engineer based in New York with a passion for making incredible web applications. Currently I am a developer at Policygenius, where I am building full stack web apps in React, Ruby on Rails, and Go. Outside of work you can find me at the gym, picking up heavy things and putting them back down. <br /> <br />If you want to get to know more about me or want to work on a project together, feel free to contact me on sunnyvmistry at gmail.com.
+          </p>
+          
+          <div className="image-container">
+            <img src="/profile-photo.jpg" />
+          </div>
+        </div>
+
+        <div className="social-icons">
+          <a href="https://github.com/sunnymis">
+            <img className="icon" src="/icons/github-logo.svg" />
+          </a>
+          <a href="https://www.linkedin.com/in/sunnymistry/">
+            <img className="icon" src="/icons/linkedin-logo.svg" />
+          </a>
+          <a href="mailto:sunny.v.mistry@gmail.com">
+            <img className="icon" src="/icons/email-icon.svg" />
+          </a>
+        </div>
       </div>
     </div>
   )
