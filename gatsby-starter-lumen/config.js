49530e670566c2c19e0a81f8ac7c1e3c423c9728
Sunny Mistry <sunny.mistry@policygenius.com>
2019-12-24 00:29:05 -0600

    Replace Sidebar with My Info

--- gatsby-starter-lumen/config.js
+++ gatsby-starter-lumen/config.js
@@ -1,48 +1,37 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://www.sunnymistry.dev',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by Sunny Mistry',
+  subtitle: 'Software Engineer',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
+  googleAnalyticsId: '', // TODO ADD THIS
   menu: [
     {
       label: 'Articles',
       path: '/'
     },
     {
-      label: 'About me',
+      label: 'About',
       path: '/pages/about'
     },
     {
-      label: 'Contact me',
+      label: 'Contact',
       path: '/pages/contacts'
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'Sunny Mistry',
+    photo: '/profile-photo.jpg',
+    bio: 'I\'m a Software Engineer based in New York who loves building web apps and weightlifting.',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: ''
+      email: 'sunny.v.mistry@gmail.com',
+      github: 'sunnymis',
+      linkedin: 'sunnymistry',
+      instagram: 'sunnmist'
     }
   }
 };
--- gatsby-starter-lumen/src/components/Sidebar/Author/Author.js
+++ gatsby-starter-lumen/src/components/Sidebar/Author/Author.js
@@ -18,8 +18,6 @@ const Author = ({ author, isIndex }: Props) => (
       <img
         src={withPrefix(author.photo)}
         className={styles['author__photo']}
-        width="75"
-        height="75"
         alt={author.name}
       />
     </Link>
