1cd9efc2951af1dfdee893c36d1d931544b2dc09
Sunny Mistry <sunny.mistry@policygenius.com>
2019-12-27 22:29:14 -0600

    Mock Moment Dates for Tests

--- src/components/Post/Content/Content.test.js
+++ src/components/Post/Content/Content.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import moment from 'moment';
 import Content from './Content';
+
+jest.mock('moment', () => {
+  const mockedMoment = {
+    format: jest.fn(),
+  };
+  return jest.fn(() => mockedMoment);
+});
 
 describe('Content', () => {
   it('renders correctly', () => {
+    moment().format.mockReturnValueOnce('December 26, 2019');
+
     const props = {
       title: 'test',
       body: '<p>test</p>'
